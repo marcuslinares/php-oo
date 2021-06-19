627930192a0003add19531f3f4fc15877ff3ccaa
Marcus Linares <marcus.linares@gmail.com>
2021-06-19 15:03:21 -0300

    Conceito de Propriedades/Atributos

--- ContaBancaria.php
+++ ContaBancaria.php
@@ -1,15 +1,15 @@
 <?php
 class ContaBancaria {
   /* Modificadores de acessos: public, private, protected*/
-  public $banco;
+  public $banco = "Itaú";
   public $nomeTitutlar = "Marcus Linares";
   public $numeroAgencia = "4032";
-  public $numeroConta;
-  public $saldo = "23,3";
+  public $numeroConta = "12345678";
+  public $saldo = 23.3;
 
 }
 
 //Uma instância
 $conta = new ContaBancaria;
 
-var_dump($conta->saldo);
+var_dump($conta);
